922c993fde804e9fa22b72d522a9217a8759c43d
geekychasser <rbothra77@gmail.com>
2020-09-06 01:25:35 +0530

    initial commit/project setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("server started on port number: ",PORT);
+})
